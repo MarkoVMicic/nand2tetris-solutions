4c0a78e83abc1019aa12a3d757ebb7527e88e541
MarkoVMicic <marko.v.micic@gmail.com>
2020-09-15 12:26:02 +0800

    Removing tabs before removing blank lines actually removes all blank lines correctly.

--- assembler/preprocess.c
+++ assembler/preprocess.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-char * preprocess_variables(char *asm_string){
+char * preprocess_variables(char *asm_string)
+{
 	// TODO(Marko): Implement
 	/*
 		I think the best way to implement this preprocessing of variables is to build a
@@ -14,12 +15,15 @@ char * preprocess_variables(char *asm_string){
 	return asm_string;
 }
 
-char * preprocess_predefined_variables(char *asm_string){
 
+char * preprocess_predefined_variables(char *asm_string)
+{
 	return asm_string;
 }
 
-char * preprocess_symbols(char *asm_string){
+
+char * preprocess_symbols(char *asm_string)
+{
 	// TODO(Marko): Implement
 	/*
 		The goal of this function is to pass over the asm string (which, by this point, 
@@ -33,13 +37,17 @@ char * preprocess_symbols(char *asm_string){
 
 }
 
-char * remove_whitespace(char *asm_string){
+
+char * remove_whitespace(char *asm_string)
+{
 	int count;
 	int i;
 
 	count = 0;
-	for(i = 0; asm_string[i]; i++){
-		if(asm_string[i] != ' '){
+	for(i = 0; asm_string[i]; i++)
+	{
+		if(asm_string[i] != ' ')
+		{
 			asm_string[count++] = asm_string[i]; 
 		}
 	}
@@ -48,13 +56,17 @@ char * remove_whitespace(char *asm_string){
 
 }
 
-char * remove_carriage_returns(char *asm_string){
+
+char * remove_carriage_returns(char *asm_string)
+{
 	int count;
 	int i;
 
 	count = 0;
-	for(i = 0; asm_string[i]; i++){
-		if(asm_string[i] != '\r'){
+	for(i = 0; asm_string[i]; i++)
+	{
+		if(asm_string[i] != '\r')
+		{
 			asm_string[count++] = asm_string[i];
 		}
 	}
@@ -62,13 +74,17 @@ char * remove_carriage_returns(char *asm_string){
 	return asm_string;
 }
 
-char * remove_tabs(char *asm_string){
+
+char * remove_tabs(char *asm_string)
+{
 	int count;
 	int i;
 
 	count = 0;
-	for(i = 0; asm_string[i]; i++){
-		if(asm_string[i] != 9){
+	for(i = 0; asm_string[i]; i++)
+	{
+		if(asm_string[i] != 9)
+		{
 			asm_string[count++] = asm_string[i];
 		}
 	}
@@ -76,13 +92,17 @@ char * remove_tabs(char *asm_string){
 	return asm_string;
 }
 
-char * remove_consecutive_newlines(char *asm_string){
+
+char * remove_consecutive_newlines(char *asm_string)
+{
 	int count;
 	int i;
 
 	count = 0;
-	for(i = 0; asm_string[i]; i++){
-		if((asm_string[i] != '\n') || ((i > 0) && (asm_string[i-1] != '\n'))){
+	for(i = 0; asm_string[i]; i++)
+	{
+		if((asm_string[i] != '\n') || ((i > 0) && (asm_string[i-1] != '\n')))
+		{
 			asm_string[count++] = asm_string[i];
 		}
 	}
@@ -90,45 +110,54 @@ char * remove_consecutive_newlines(char *asm_string){
 	return asm_string;
 }
 
-char * remove_blank_lines(char *asm_string){
+
+char * remove_blank_lines(char *asm_string)
+{
 	asm_string = remove_carriage_returns(asm_string);
 	asm_string = remove_consecutive_newlines(asm_string);
 	
 	return asm_string;
 }
 
+
 char * remove_comments(char *asm_string){
 	// ASM comments are identical to single-line comments in C. 
 	int i;
 	int j;
 	int k;
-	for(i = 0; asm_string[i]; i++){
-		if(asm_string[i] == '/'){			// First slash
-			if(asm_string[i+1] == '/'){	// Second slash indicates comment!
+	for(i = 0; asm_string[i]; i++)
+	{
+		if(asm_string[i] == '/')
+		{			// First slash
+			if(asm_string[i+1] == '/')
+			{	// Second slash indicates comment!
 				// Ignore until end of line
 				j = i;
-				while(asm_string[j] != '\n'){
+				while(asm_string[j] != '\n')
+				{
 					j++;
 				}
-				for(k=0; k < j-i; k++){
+				for(k=0; k < j-i; k++)
+				{
 					asm_string[i+k] = ' ';
 				}
 			}
 		}
 	}
-	asm_string = remove_whitespace(asm_string);
-	asm_string = remove_blank_lines(asm_string);
 	return asm_string;
 }
 
-char * process_asm_string(char *asm_string){
-	asm_string = remove_whitespace(asm_string);
-	asm_string = remove_blank_lines(asm_string);
-	asm_string = remove_tabs(asm_string);
+
+char * process_asm_string(char *asm_string)
+{
 	asm_string = remove_comments(asm_string);
+	asm_string = remove_whitespace(asm_string);
+	asm_string = remove_tabs(asm_string);
+	asm_string = remove_blank_lines(asm_string);
 	// TODO: Handle variables here.
 	return asm_string;
 }
+
 
 char * open_file_store_as_string(const char *file_path)
 {
@@ -138,7 +167,8 @@ char * open_file_store_as_string(const char *file_path)
 
 	printf("Parsing file found at %s ...\n", file_path);
 	file = fopen(file_path, "r");
-	if(file){	// i.e. if file != NULL
+	if(file)
+	{	// i.e. if file != NULL
 		fseek(file, 0, SEEK_END);
 		length = ftell(file);
 		fseek(file, 0, SEEK_SET);
@@ -149,7 +179,8 @@ char * open_file_store_as_string(const char *file_path)
 		  }
 		  fclose(file);
 	}
-	else{
+	else
+	{
 		asm_string = malloc(1);
 		asm_string[0] = '\0';
 	}
