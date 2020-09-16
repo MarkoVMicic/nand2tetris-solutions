fb90eec996cd94a1d06cbe1b0109cf2fe006a811
MarkoVMicic <marko.v.micic@gmail.com>
2020-09-16 15:14:28 +0800

    Initialized variable table with predefined variables.

--- assembler/preprocess.c
+++ assembler/preprocess.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "linked_list.h"
 
-char * preprocess_variables(char *asm_string)
+void insert_predefined_symbols_into_variable_table(linked_list * head)
 {
-	// TODO(Marko): Implement
-	/*
-		I think the best way to implement this preprocessing of variables is to build a
-		hash table which uses some hash function to convert a variable length string into
-		a definitive address. 
+	head = initialize_linked_list(head);
+	append_entry_to_end_of_list(head, "R0", 0);
+	append_entry_to_end_of_list(head, "R1", 1);
+	append_entry_to_end_of_list(head, "R2", 2);
+	append_entry_to_end_of_list(head, "R3", 3);
+	append_entry_to_end_of_list(head, "R4", 4);
+	append_entry_to_end_of_list(head, "R5", 5);
+	append_entry_to_end_of_list(head, "R6", 6);
+	append_entry_to_end_of_list(head, "R7", 7);
+	append_entry_to_end_of_list(head, "R8", 8);
+	append_entry_to_end_of_list(head, "R9", 9);
+	append_entry_to_end_of_list(head, "R10", 10);
+	append_entry_to_end_of_list(head, "R11", 11);
+	append_entry_to_end_of_list(head, "R12", 12);
+	append_entry_to_end_of_list(head, "R13", 13);
+	append_entry_to_end_of_list(head, "R14", 14);
+	append_entry_to_end_of_list(head, "R15", 15);
 
-
-	*/
-	return asm_string;
+	append_entry_to_end_of_list(head, "SCREEN", 16384);
+	append_entry_to_end_of_list(head, "KBD", 24576);
+	append_entry_to_end_of_list(head, "SP", 0);
+	append_entry_to_end_of_list(head, "LCL", 1);
+	append_entry_to_end_of_list(head, "ARG", 2);
+	append_entry_to_end_of_list(head, "THIS", 3);
+	append_entry_to_end_of_list(head, "THAT", 4);
+	// remove initializing head value
+	remove_entry_from_beginning_of_list(&head);
+	print_linked_list(head);
 }
-
-
-char * preprocess_predefined_variables(char *asm_string)
-{
-	return asm_string;
-}
-
 
 char * preprocess_symbols(char *asm_string)
 {
-	// TODO(Marko): Implement
-	/*
-		The goal of this function is to pass over the asm string (which, by this point, 
-		consists of a single instruction per line with no comments), and to process 
-		the two types of symbols: variables and labels. We want to essentially replace
-		these variables and symbols with appropriate values.
-
-		We also want to process special values known as pre-defined symbols
-	*/
+	linked_list * variable_table; 
+	// initialize variable_table with predefined symbols
+	insert_predefined_symbols_into_variable_table(variable_table);
 	return asm_string;
-
 }
 
 
@@ -43,8 +49,10 @@ char * remove_whitespace(char *asm_string)
 	int count;
 	int i;
 
+
 	count = 0;
 	for(i = 0; asm_string[i]; i++)
+	for(i = 0; asm_string[i] != '\0'; i++)
 	{
 		if(asm_string[i] != ' ')
 		{
@@ -120,11 +128,13 @@ char * remove_blank_lines(char *asm_string)
 }
 
 
-char * remove_comments(char *asm_string){
+char * remove_comments(char *asm_string)
+{
 	// ASM comments are identical to single-line comments in C. 
 	int i;
 	int j;
 	int k;
+
 	for(i = 0; asm_string[i]; i++)
 	{
 		if(asm_string[i] == '/')
@@ -154,6 +164,7 @@ char * process_asm_string(char *asm_string)
 	asm_string = remove_whitespace(asm_string);
 	asm_string = remove_tabs(asm_string);
 	asm_string = remove_blank_lines(asm_string);
+	asm_string = preprocess_symbols(asm_string);
 	// TODO: Handle variables here.
 	return asm_string;
 }
@@ -183,6 +194,7 @@ char * open_file_store_as_string(const char *file_path)
 	{
 		asm_string = malloc(1);
 		asm_string[0] = '\0';
+		printf("something went wrong.\n");
 	}
 	// printf("%s\n", asm_string);
 	return asm_string;
