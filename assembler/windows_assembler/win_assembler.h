b602daf35359ee7e5b2faaf4301de44168866c85
MarkoVMicic <marko.v.micic@gmail.com>
2021-12-01 03:01:46 +0800

    Add some int typedefs to simplify work going forward

--- assembler/windows_assembler/win_assembler.h
+++ assembler/windows_assembler/win_assembler.h
@@ -1,5 +1,7 @@
 #if !defined(WIN_ASSEMBLER_H)
 
+#include <stdint.h>
+#include <stddef.h>
 
 // NOTE: We are putting these #define in the code to make the code more 
 //       searchable. Now we can more easily search for global variables, 
@@ -10,6 +12,17 @@
 #define local_persist static
 #define global_variable static 
 
+typedef int8_t int8; 
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef int32 bool32;
+
+// NOTE: unsigned char is the same as an 8-bit integer. 
+typedef uint8_t uint8;  
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 
 
