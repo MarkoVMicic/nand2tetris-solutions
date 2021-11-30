3a818129f7a98f3ff32f244c893e33b94aec39fc
MarkoVMicic <marko.v.micic@gmail.com>
2021-12-01 03:14:20 +0800

    Finish the rest of the preprocessing.
        Removal of whitespace and extraneous carriage returns etc.

--- assembler/windows_assembler/preprocess.cpp
+++ assembler/windows_assembler/preprocess.cpp
@@ -28,11 +28,80 @@ internal void RemoveComments(asm_string *AsmString)
 }
 
 
+internal void RemoveWhiteSpace(asm_string *AsmString)
+{
+    int Count = 0;
+    int RemovedCharCount = 0;
+    for(int i = 0; i < AsmString->Length; i++)
+    {
+        // NOTE(Marko): If we encounter a space, we will remove it. This shortens the string length. If we don't encounter a space, we'll copy whatever is at the ith position into the count position. 
+        if(AsmString->Contents[i] != ' ')
+        {
+            AsmString->Contents[Count++] = AsmString->Contents[i];
+        }
+        else
+        {
+            RemovedCharCount++;
+        }
+    }
+    // NOTE(Marko): Need to null terminate the string.
+    AsmString->Contents[Count] = '\0';
+    AsmString->Length -= RemovedCharCount;
+}
+
+
+internal void RemoveTabs(asm_string *AsmString)
+{
+    int Count = 0;
+    int RemovedCharCount = 0;
+    for(int i = 0; i < AsmString->Length; i++)
+    {
+        if(AsmString->Contents[i] == '\t')
+        {
+            RemovedCharCount++;
+        }
+        else
+        {
+            AsmString->Contents[Count++] = AsmString->Contents[i];
+        }
+    }
+    AsmString->Contents[Count] = '\0';
+    AsmString->Length -= RemovedCharCount;
+}
+
+
+internal void RemoveBlankLines(asm_string *AsmString)
+{
+    int Count = 0;
+    int RemovedCharCount = 0;
+    for(int i = 0; i < AsmString->Length; i++)
+    {
+        // NOTE(Marko): Remove Carriage Returns
+        if(AsmString->Contents[i] == '\r')
+        {
+            RemovedCharCount++;
+        }
+        // NOTE(Marko): Remove consecutive newlines
+        else if((i > 0) && (AsmString->Contents[i] == '\n') && AsmString->Contents[i-1] == '\n')
+        {
+            RemovedCharCount++;
+        }
+        else
+        {
+            AsmString->Contents[Count++] = AsmString->Contents[i];
+        }
+    }
+    // NOTE(Marko): Need to null terminate the string.
+    AsmString->Contents[Count] = '\0';
+    AsmString->Length -= RemovedCharCount;
+}
 
 
 internal void PreprocessAsmString(asm_string *AsmString)
 {
-
+    // TODO(Marko): Should be able to do all of this in a single pass instead of multiple passes like this. 
     RemoveComments(AsmString);
-
+    RemoveWhiteSpace(AsmString);
+    RemoveTabs(AsmString);
+    RemoveBlankLines(AsmString);
 }
