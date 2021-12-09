025dad04994c6a94611b4144a0aa26a1aadc6fe9
MarkoVMicic <marko.v.micic@gmail.com>
2021-12-09 20:29:58 +0800

    Add the definitions to the header file that were missing.
        Oops, forgot to commit these earlier .

--- assembler/windows_assembler/preprocess.h
+++ assembler/windows_assembler/preprocess.h
@@ -6,6 +6,14 @@
 #define TAB '\t'
 #define CARRIAGE_RETURN '\r'
 #define NEWLINE '\n'
+#define OPEN_BRACKET '('
+#define CLOSED_BRACKET ')'
+
+// NOTE(Marko): Specified by nand2tetris to start here. 
+#define USER_DEFINED_VAR_ADDRESS_START 16
+#define USER_DEFINED_VAR_ADDRESS_END 15000
+#define UNINITIALIZED_ADDRESS 65535
+
 
 #define PREPROCESS_H
 #endif
