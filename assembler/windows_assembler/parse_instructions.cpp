f560c74660b9ab4e65651167b8d00adde49581b7
MarkoVMicic <marko.v.micic@gmail.com>
2021-12-11 02:15:10 +0800

    Add simple power function for exponentiation of uint32

--- assembler/windows_assembler/parse_instructions.cpp
+++ assembler/windows_assembler/parse_instructions.cpp
@@ -1,6 +1,55 @@
 #include "parse_instructions.h"
 
 internal void ParseInstructions(asm_string *PreprocessedAsmString,
+// TODO(Marko): Can make this a lot better using square and multiply algorithm 
+//              on the bits than this multiply but it's probably not that 
+//              important
+inline uint32 Pow(uint32 Base, uint32 Exponent)
+{
+    uint32 Result = 0;
+    if(Base == 0 && Exponent == 0)
+    {
+        // NOTE(Marko): This is undefined, we shouldn't be calling into this 
+        //              function like this! 
+        InvalidCodePath;
+    }
+    else if(Base == 0)
+    {
+        Result = 0;
+    }
+    else if(Exponent == 0)
+    {
+        Result = 1;
+    }
+    else if(Exponent == 1)
+    {
+        Result = Base;
+    }
+    else if(Exponent == 2)
+    {
+        Result = Base*Base;
+    }  
+    else if(Exponent == 3)
+    {
+        Result = Base*Base*Base;
+    }  
+    else if(Exponent == 4)
+    {
+        Result = Base*Base*Base*Base;
+    }      
+    else
+    {
+        for(uint32 i = 0; i < Exponent; i++)
+        {
+            Result *= Base;
+        }
+    }
+
+    return(Result);
+}
+
+
+
                                 asm_string *MachineCodeAsmString,
                                 variable_table *UserDefinedVariableTable)
 {
