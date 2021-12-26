71a7d5e30b8d071da488ba89329f23a794766419
MarkoVMicic <marko.v.micic@gmail.com>
2021-12-26 17:29:51 +0200

    Flesh out vm_main header with int sizes, typedefs for internal code, read_file_result.

--- vm/vm_main.h
+++ vm/vm_main.h
@@ -1,7 +1,29 @@
 #if !defined(VM_MAIN_H)
 
+#include <stdint.h>
 #include <stddef.h>
 #include <stdio.h>
+
+// NOTE: We are putting these #define in the code to make the code more 
+//       searchable. Now we can more easily search for global variables, 
+//       locally-persistent variables, and internal functions much more easily 
+//       than if these three vastly different types were all indicated with 
+//       the same static keyword. 
+#define internal static    
+#define local_persist static
+#define global_variable static 
+
+typedef int8_t int8; 
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef int32 bool32;
+
+// NOTE: unsigned char is the same as an 8-bit integer. 
+typedef uint8_t uint8;  
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 #if VM_DEBUG
     // NOTE: Assert macro -> Write to the NULL pointer if Expression is false. 
@@ -15,6 +37,12 @@
 #define InvalidCodePath Assert(!"InvalidCodePath");
 
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
+
+struct read_file_result
+{
+    char *Contents;
+    uint32 Length;
+};
 
 #define VM_MAIN_H
 #endif
